7233cf1689b02a76be8e30781f2e627198720fce
Pavan I <139200578+pavan-furation@users.noreply.github.com>
2023-10-03 20:02:29 +0530

    Revert "Feature/realtor (#5)"
    This reverts commit 172c3864a7e8c30b79248199fed8ce286d65a8c1.

--- src/domain/aws/repositories/aws-repository.ts
+++
@@ -1,7 +0,0 @@
-import { Either } from "monet";
-import  ErrorClass from "@presentation/error-handling/api-error";
-
-export interface MediaRepository {
-  getPreSignedUrl(media: string): Promise<Either<ErrorClass, string>>;
-  // deleteBrandLogo(): Promise<Either<ErrorClass, string>>;
-}
--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,7 +1,6 @@
 
 import { connectionsRouter } from "@presentation/routes/connections";
-import { realtorRouter } from "@presentation/routes/realtor-routes";
-import { faqsRouter } from "@presentation/routes/faqs-routes";
+import { clientTagCategoryRouter } from "@presentation/routes/client-tag-category-route";
 
 import { type Express, Router } from "express";
 
@@ -16,11 +15,8 @@ export default (app: Express): void => {
     res.status(200).json({ message: "ok" });
   });
 
-  
   app.use("/api/v1/connections", connectionsRouter);
   app.use("/api/v1/connection", clientTagCategoryRouter);
-  app.use("/api/v1/realtor", realtorRouter);
-  app.use("/api/v1/faqs", faqsRouter);
 
   app.use(router);
 };
