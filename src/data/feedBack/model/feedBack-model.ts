199e27b4faae9da9b6f0a5561c388fe209c79dfa
Survesh Pandit <survesh.pandit@furation.tech>
2023-10-05 13:12:46 +0530

    feat: done the feedBack Schema

--- src/data/feedBack/model/feedBack-model.ts
+++ src/data/feedBack/model/feedBack-model.ts
@@ -12,22 +12,22 @@ const FeedBacks = sequelize.define('FeedBacks', {
 });
 
 // Define the association between FeedBacks and Realtor
-FeedBacks.belongsTo(Realtors, {
-  foreignKey: 'fromRealtor', // The name of the foreign key column in FeedBacks
-  targetKey: 'id' // The name of the target key column in Realtor
-});
+// FeedBacks.belongsTo(Realtors, {
+//   foreignKey: 'fromRealtor', // The name of the foreign key column in FeedBacks
+//   targetKey: 'id' // The name of the target key column in Realtor
+// });
 
 // Define the association between FeedBacks and Realtor
-FeedBacks.belongsTo(Realtors, {
-  foreignKey: 'toRealtor', // The name of the foreign key column in FeedBacks
-  targetKey: 'id' // The name of the target key column in Realtor
-});
+// FeedBacks.belongsTo(Realtors, {
+//   foreignKey: 'toRealtor', // The name of the foreign key column in FeedBacks
+//   targetKey: 'id' // The name of the target key column in Realtor
+// });
 
 // Define the association between FeedBacks and Realtor
-FeedBacks.belongsTo(Jobs, {
-  foreignKey: 'jobId', // The name of the foreign key column in FeedBacks
-  targetKey: 'id' // The name of the target key column in Realtor
-});
+// FeedBacks.belongsTo(Jobs, {
+//   foreignKey: 'jobId', // The name of the foreign key column in FeedBacks
+//   targetKey: 'id' // The name of the target key column in Realtor
+// });
 
 (async () => {
   await sequelize.sync({ force: false });
--- src/domain/blocking/usecases/blocking-root.ts
+++
@@ -1,5 +0,0 @@
-import { CreateBlocking } from "./create-blocking";
-
-module.exports = {
-    CreateBlocking
-};
