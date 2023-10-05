b4dac970aeaabffdc41ba5357fd1dc7790f9caa6
MansiSableFuration <mansi.sabale@furation.tech>
2023-10-05 17:00:14 +0530

    feat: model and entity of Support implemented

---
+++ src/data/support/models/support-model.ts
@@ -0,0 +1,43 @@
+import { DataTypes } from "sequelize";
+import sequelize from "@main/sequelizeClient";
+
+const Support = sequelize.define("Support", {
+  realtor: {
+    type: DataTypes.UUID,
+    allowNull: false,
+    // references: {
+    //   model: "Realtors", // Make sure this matches your "Jobs" model name
+    //   key: "id", // Adjust this key if necessary
+    // },
+  },
+  to: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      len: {
+        args: [1, 1000],
+        msg: "Description length must be between 1 and 1000 characters",
+      },
+    },
+  },
+  attachments: {
+    type: DataTypes.ARRAY(DataTypes.STRING), // PostgreSQL ARRAY type for attachments
+    allowNull: true, // Change to false if it should not be nullable
+  },
+  timestamp: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+  },
+});
+
+(async () => {
+  await sequelize.sync({ force: false });
+  // Code here
+})();
+
+export default Support;
