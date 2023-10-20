8dc7a8ac81d9035173f13795e8fe54707d7adef3
Pavan I <139200578+pavan-furation@users.noreply.github.com>
2023-10-20 10:54:55 +0530

    Feature/connection f (#66)
    * feat: join function

--- src/data/connections/datasource/connections_datasource.ts
+++ src/data/connections/datasource/connections_datasource.ts
@@ -43,7 +43,8 @@ export class ConnectionsDataSourceImpl implements ConnectionsDataSource {
 
     async deleteReq(id: string): Promise<void> {
         // Find the record by ID
-        const connection: any = await Connections.findByPk(id);
+        let num = parseInt(id);
+        const connection: any = await Connections.findByPk(num);
         // Update the record with the provided data
 
         const from: any = await Realtors.findByPk(connection.fromId);
@@ -84,22 +85,55 @@ export class ConnectionsDataSourceImpl implements ConnectionsDataSource {
     }
 
     async read(id: string): Promise<any | null> {
+        let num = parseInt(id);
         const connections = await Connections.findOne({
             where: {
-                id: id
+                id: num,
+                connected: false
             },
+            include: [{
+                model: Realtors,
+                as: 'from', // Alias for the first association
+                foreignKey: 'fromId',
+            },
+            {
+                model: Realtors,
+                as: 'to', // Alias for the second association
+                foreignKey: 'toId',
+            },
+            ],
         });
         return connections ? connections.toJSON() : null; // Convert to a plain JavaScript object before returning
     }
 
     async getAll(): Promise<any[]> {
-        const connections = await Connections.findAll();
-        return connections.map((connection: any) => connection.toJSON()); // Convert to plain JavaScript objects before returning
+
+        //    const state = req.query.q as string;
+        //         const RealtorID = req.body.realtorID as number;
+
+        const data = await Connections.findAll({
+            // include: [{
+            //     model: Realtors,
+            // }],
+            include: [{
+                model: Realtors,
+                as: 'from', // Alias for the first association
+                foreignKey: 'fromId',
+            },
+            {
+                model: Realtors,
+                as: 'to', // Alias for the second association
+                foreignKey: 'toId',
+            },
+            ],
+        });
+        return data.map((connection: any) => connection.toJSON()); // Convert to plain JavaScript objects before returning
     }
 
     async updateReq(id: string, updatedData: ConnectionsModel): Promise<any> {
         // Find the record by ID
-        const connection: any = await Connections.findByPk(id);
+        let num = parseInt(id);
+        const connection: any = await Connections.findByPk(num);
         // Update the record with the provided data
 
         await connection.update(updatedData);
@@ -129,145 +163,3 @@ export class ConnectionsDataSourceImpl implements ConnectionsDataSource {
 
 }
 
-
-
-// import { Sequelize, where } from "sequelize"
-// import { ConnectionsModel } from "@domain/connections/entities/connections_entities"; // Import the connectionsModel
-// import Connections from "../models/connections_model";
-// import ApiError from "@presentation/error-handling/api-error";
-// import Realtors from "@data/realtors/model/realtor-model";
-
-// // Create ConnectionsDataSource Interface
-// export interface ConnectionsDataSource {
-//     createReq(connections: ConnectionsModel): Promise<any>;
-//     updateReq(id: string, data: ConnectionsModel): Promise<any>;
-//     deleteReq(id: string): Promise<void>;
-//     read(id: string): Promise<any | null>;
-//     getAll(): Promise<any[]>;
-
-// }
-
-// // Connections Data Source communicates with the database
-// export class ConnectionsDataSourceImpl implements ConnectionsDataSource {
-//     constructor(private db: Sequelize) { }
-
-//     async createReq(newConnection: any): Promise<any> {
-
-//         const existingConnection1 = await Connections.findOne({
-//             where: {
-//                 fromId: newConnection.fromId,
-//                 toId: newConnection.toId
-//             }
-//         });
-
-//         const existingConnection2 = await Connections.findOne({
-//             where: {
-//                 fromId: newConnection.toId,
-//                 toId: newConnection.fromId
-//             },
-//         });
-
-//         if (existingConnection1 || existingConnection2) {
-//             throw ApiError.emailExist();
-//         }
-//         const createdConnections = await Connections.create(newConnection);
-//         return createdConnections.toJSON();
-//     }
-
-//     async deleteReq(id: string): Promise<void> {
-//         // Find the record by ID
-//         const connection: any = await Connections.findByPk(id);
-//         // Update the record with the provided data
-
-//         const from: any = await Realtors.findByPk(connection.fromId);
-//         const to: any = await Realtors.findByPk(connection.toId);
-
-//         // from.friends.push(updatedData.toId);
-//         for (let i = 0; i < from.friends.length; i++) {
-//             if (from.friends[i] == connection.toId) {
-//                 from.friends.splice(i, 1);
-//             }
-//         }
-
-//         await Realtors.update(from.dataValues, {
-//             where: {
-//                 id: from.id,
-//             }
-//         });
-
-//         for (let i = 0; i < to.friends.length; i++) {
-//             if (to.friends[i] == connection.fromId) {
-//                 to.friends.splice(i, 1);
-//             }
-//         }
-
-//         await Realtors.update(to.dataValues, {
-//             where: {
-//                 id: to.id,
-//             }
-//         });
-
-
-
-//         await Connections.destroy({
-//             where: {
-//                 id: id
-//             },
-//         });
-//     }
-
-//     async read(id: string): Promise<any | null> {
-//         const connections = await Connections.findOne({
-//             where: {
-//                 id: id
-//             },
-//             include: [
-//                 {
-//                   model: Realtors,
-//                   as: 'fromRealtor'
-//                 },
-//                 {
-//                   model: Realtors,
-//                   as: 'toRealtor'                
-//                 },
-//               ],
-//         });
-//         return connections ? connections.toJSON() : null; // Convert to a plain JavaScript object before returning
-//     }
-
-//     async getAll(): Promise<any[]> {
-//         const connections = await Connections.findAll();
-//         return connections.map((connection: any) => connection.toJSON()); // Convert to plain JavaScript objects before returning
-//     }
-
-//     async updateReq(id: string, updatedData: ConnectionsModel): Promise<any> {
-//         // Find the record by ID
-//         const connection: any = await Connections.findByPk(id);
-//         // Update the record with the provided data
-
-//         await connection.update(updatedData);
-//         await connection.save();
-
-//         const from: any = await Realtors.findByPk(updatedData.fromId);
-//         from.friends.push(updatedData.toId);
-//         await Realtors.update(from.dataValues, {
-//             where: {
-//                 id: from.id,
-//             }
-//         });
-
-//         const to: any = await Realtors.findByPk(updatedData.toId);
-//         to.friends.push(updatedData.fromId);
-//         await Realtors.update(to.dataValues, {
-//             where: {
-//                 id: to.id,
-//             }
-//         });
-
-//         const updatedConnections = await Connections.findByPk(id);
-
-//         return updatedConnections ? updatedConnections.toJSON() : null; // Convert to a plain JavaScript object before returning
-//     }
-
-
-// }
--- src/data/connections/models/connections_model.ts
+++ src/data/connections/models/connections_model.ts
@@ -1,37 +1,3 @@
-// import { strict } from "assert";
-// import { DataTypes } from "sequelize";
-// import env from '@main/config/env';
-// import { array, boolean, object, string } from "joi";
-// import sequelize from "@main/sequelizeClient";
-
-// import Realtors from "@data/realtors/model/realtor-model";
-
-
-// const Connections = sequelize.define('Connections', {
-//   fromId: {
-//     type: DataTypes.INTEGER,
-//     allowNull: false,
-//     references: { model: Realtors, key: 'id' }
-//   },
-//   toId: {
-//     type: DataTypes.INTEGER,
-//     allowNull: false,
-//     references: { model: Realtors, key: 'id' }
-//   },
-//   connected: {
-//     type: DataTypes.BOOLEAN,
-//     defaultValue: false,
-//   },
-// });
-
-// Realtors.hasMany(Connections);
-// Connections.belongsTo(Realtors, { foreignKey: 'fromId', as: 'fromRealtor' });
-// Connections.belongsTo(Realtors, { foreignKey: 'toId', as: 'toRealtor' });
-
-// export default Connections;
-
-
-
 import { strict } from "assert";
 import { DataTypes } from "sequelize";
 import env from '@main/config/env';
@@ -58,14 +24,17 @@ const Connections = sequelize.define('Connections', {
   },
 });
 
-// Connections.belongsTo(Realtors, { foreignKey: 'fromId', as: 'fromRealtor' });
-// Connections.belongsTo(Realtors, { foreignKey: 'toId', as: 'toRealtor' });
+Connections.belongsTo(Realtors, {
+    foreignKey: "fromId",
+    as: "from" // Alias for the first association
+});
 
-// Connections.belongsToMany(Realtors, { foreignKey: 'fromId', as: 'fromRealtor' });
-// Connections.belongsTo(Realtors, { foreignKey: 'toId', as: 'toRealtor' });
+Connections.belongsTo(Realtors, {
+    foreignKey: "toId",
+    as: "to" // Alias for the second association
+});
 
-// Realtors.hasMany(Connections, { foreignKey: 'fromId', as: 'fromConnections' });
-// Realtors.hasMany(Connections, { foreignKey: 'toId', as: 'toConnections' });
+
 
 export default Connections;
 
