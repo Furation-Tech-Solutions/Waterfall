492b28635235976cfdf2970c3fc833e7b30756d1
Mansi Sable <133741631+MansiSableFuration@users.noreply.github.com>
2023-12-18 13:49:09 +0530

    Cleaningthecode (#231)
    * fix: modified deleteStatus in Realtor
    
    * fix: changed job schema and firebas-sdk file
    
    ---------
    
    Co-authored-by: MansiSanjaySable <133741631+MansiSanjaySable@users.noreply.github.com>

--- src/presentation/middlewares/authentication/authentication-middleware.ts
+++ src/presentation/middlewares/authentication/authentication-middleware.ts
@@ -1,37 +1,30 @@
 import { Request, Response, NextFunction, RequestHandler } from "express";
-import admin from "../../../main/firebase-sdk/firebase-config"
+import admin from "../../../main/firebase-sdk/firebase-config";
 // import serviceAccount from "../../../../"
 import * as path from "path";
+import ApiError from "@presentation/error-handling/api-error";
 
+export const verifyUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<any> => {
+  const authHeader: string | undefined = req.header("Authorization");
 
-
- export const verifyUser=async(
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ): Promise<any> => {
-      const authHeader: string | undefined = req.header("Authorization");
-
-  try {
-    if (!authHeader || !authHeader.startsWith("Bearer ")) {
-      throw new Error("Unauthorized");
-    }
-
-    const idToken = authHeader.split(" ")[1]; // Extracting the token part after "Bearer "
-
-    const user = await admin.auth().verifyIdToken(idToken);
-
-    if (user) {
-      req.user = user.uid;
-      next();
-    } else {
-      throw new Error("Unauthorized");
-    }
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    // throw ApiError.customError(401,"you are not authorized");
+    return res.status(401).json({ message: "You are not Authorized" });
   }
-    
-    catch(err){
-     res.status(401).send("Unauthorized");
-    // console.log(err)
-   }
 
-}
+  const idToken = authHeader.split(" ")[1]; // Extracting the token part after "Bearer "
+
+  const user = await admin.auth().verifyIdToken(idToken);
+
+  if (user) {
+    req.user = user.uid;
+    next();
+  } else {
+    // throw ApiError.unAuthorized()
+    return res.status(401).json({ message: "You are not Authorized" });
+  }
+};
