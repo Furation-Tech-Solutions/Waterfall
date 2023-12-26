25414b1e0660df7fb425a2dbca4d9ef0a5efb2d7
MalikFuration19 <139318742+MalikFuration19@users.noreply.github.com>
2023-12-26 16:00:32 +0530

    Feature/notification impl (#259)
    * feat: updated user auth
    
    * feat: updated error for single realtor
    
    * feat: added notification functionality
    
    * feat: update error handling in user authentication
    
    * refactor: implemented push notification
    
    * feat: added job applicant push notification
    
    * feat: updated push notification
    
    ---------
    
    Co-authored-by: Malik04121 <shehzadmalik123.sm@gmail.com>

--- src/presentation/services/push-notification-services.ts
+++ src/presentation/services/push-notification-services.ts
@@ -1,11 +1,138 @@
 // const admin = require('firebase-admin');
 import * as path from "path";
 import admin from "../../main/firebase-sdk/firebase-config"
+import { RealtorDataSourceImpl } from "@data/realtors/datasources/realtor-data-source";
+import { sequelize } from "@main/sequelizeClient";
+import { NotificationDataSourceImpl } from "@data/notification/datasource/notification-datasource";
+import { JobDataSourceImpl } from "@data/job/datasources/job-data-sources";
 
-// Assuming serviceAccount.json is in the app folder
 export class NotificationSender {
-    constructor() {
-        // Fetch the service account key JSON file content
+  private realtorDataSource: RealtorDataSourceImpl;
+  private jobDataSource: JobDataSourceImpl;
+  // private notificationSender: NotificationSender;
+  
+  constructor() {
+    this.realtorDataSource = new RealtorDataSourceImpl(sequelize);
+    this.jobDataSource = new JobDataSourceImpl(sequelize);
+    // this.notificationSender = new NotificationSender();
+  }
+
+  async getSender(eventType:string, senderId:any) {
+    let dataSource:any;
+  
+    switch (eventType) {
+      case 'connectionRequest':
+        return await this.realtorDataSource.read(senderId)
+
+      case 'connectionRequestResponse':
+          return await this.realtorDataSource.read(senderId)
+
+      case 'feedback':
+        return await this.realtorDataSource.read(senderId)
+
+      case 'appliedJob':
+        return await this.realtorDataSource.read(senderId)
+
+        case 'sendMessage':
+          return await this.realtorDataSource.read(senderId)
+      // Add other cases for different event types
+      case 'applicantStatusAccept':
+        return await this.realtorDataSource.read(senderId)
+
+        case 'applicantStatusDecline':
+        return await this.realtorDataSource.read(senderId)
+  
+      default:
+        throw new Error('Invalid event type');
+    }
+  
+   
+  }
+  
+ 
+
+  async getReceiver(eventType:string, receiverId:any) {
+
+    switch (eventType) {
+      case 'connectionRequest':
+        return await this.realtorDataSource.read(receiverId)
+
+      case 'connectionRequestResponse':
+          return await this.realtorDataSource.read(receiverId)
+
+      case 'feedback':
+        return await this.realtorDataSource.read(receiverId)
+
+      case 'appliedJob':
+        return await this.jobDataSource.read(receiverId)
+
+        case 'applicantStatusAccepted':
+        return await this.realtorDataSource.read(receiverId)
+
+        case 'applicantStatusDecline':
+          return await this.realtorDataSource.read(receiverId)
+
+      
+        case 'sendMessage':
+          return await this.realtorDataSource.read(receiverId)
+  
+      // Add other cases for different event types
+  
+      default:
+        throw new Error('Invalid event type');
+    }
+   }
+      // Implement specific receiver logic for different event types
+      
+    
+  
+
+  async getTitleAndBody(eventType:string, sender:any, receiver:any) {
+    let title = '';
+    let body = '';
+    switch (eventType) {
+      case 'connectionRequest':
+        title = 'Connection Request';
+        body = `You have a connection request from ${sender.firstName ?? 'Abhishek'}`;
+        break;
+
+      case 'connectionRequestResponse':
+        title = 'Connection Request Status';
+        body = `${sender.firstName ?? 'user'} has accepted your friend request`;
+        break;
+
+      case 'feedback':
+        title = 'Feedback on job';
+        body = `${sender.firstName ?? 'user'} has provided feedback on your job`;
+        break;
+
+      case 'appliedJob':
+        title = 'Applied Job Status';
+        body = `${sender.firstName ?? 'user'}${sender.lastName ?? ''} has requested you on your recent post`;
+        break;
+      
+        case 'sendMessage':
+        title = 'Message Request';
+        body = `You have message request from ${sender.firstName ?? "user"} ${sender.lastName ?? ''}`;
+        break;
+
+        case 'applicantStatusAccepted':
+        title = 'applied job status';
+        body = `Your application has been selected for the job role.Tap to view job agreement`;
+        break;
+
+        case 'applicantStatusDecline':
+        title = 'applied job status';
+        body = `Your application has been Rejected for the job role.`;
+        break;
+
+      default:
+        title = 'Default Title';
+        body = 'Default Body';
+        break;
+    }
+
+    return { title, body };
   }
 
   sendNotification(registrationToken:string, title:string, body:string) {
@@ -20,22 +147,67 @@ export class NotificationSender {
     return admin.messaging().send(message);
   }
 
+  async sendInAppNotification(senderId:string,receiverId:string,body:string){
+    try{
+  let notificationDataSource = new NotificationDataSourceImpl(sequelize)
+
+       const notificationData={
+          senderId,
+          receiverId,
+          message:body
+       }
+      const inAppNotifData=await notificationDataSource.createNotification(notificationData)
+    }
+    catch(error){
+      console.error("Error sending in-app notification:", error);
+
+    }
+
+  }
   
+
+  async customNotification(senderId:any, receiverId:any, eventType:string) {
   
-  
-  customNotification(){
-  // Example usage:
-  const notificationSender = new NotificationSender();
-  
-  // Sending a notification
-  const registrationToken:string = "cJtZn4tB0PXV_KYBzkDwj7:APA91bHjrMmfQp88NR8PMlpOE1QFDmYKHOrhIIBzgX3wXubqBwMcZCvolH8nCFt2bfh0wShl67bY_SDMZPSC7ilnIQw70FNBZEl9ZqX3FL5PWSCv5jE-eEojqbtFkla8G5J3pI6-VvaG";
-  notificationSender.sendNotification(registrationToken, 'Custom Title', 'Custom Body')
-  .then((response:any) => {
-      console.log('Notification sent successfully:', response);
-    })
-    .catch((error:any) => {
-        console.error('Error sending notification:', error);
-    });
+    let sender:any
+    let receiver:any
+     sender = await this.getSender(eventType, senderId);
+     receiver = await this.getReceiver(eventType, receiverId);
+
+    const { title, body } = await this.getTitleAndBody(eventType, sender, receiver);
+    let deviceTokens:any
+ 
+    if (receiver) {
+      if ('firebaseDeviceToken' in receiver) {
+        deviceTokens = receiver.firebaseDeviceToken ;
+      } else if ( 'jobOwnerData' in receiver &&  typeof receiver.jobOwnerData=== 'object'  &&'firebaseDeviceToken' in receiver.jobOwnerData!
+      ) {
+        deviceTokens = receiver.jobOwnerData.firebaseDeviceToken || [];
+      }
+    }
+
+    // const deviceTokens = receiver?.firebaseDeviceToken;
+
+    if (deviceTokens && deviceTokens.length > 0) {
+      deviceTokens.forEach((registrationToken:string) => {
+        this.sendNotification(registrationToken, title, body)
+          .then((response:any) => {
+            console.log('Notification sent successfully:', response);
+          })
+          .catch((error:any) => {
+            console.error('Error sending notification:', error);
+          });
+
+        // call inappnotification
+        if(eventType=="appliedJob"){
+          receiverId=receiver.jobOwnerId
+        }
+        this.sendInAppNotification(senderId, receiverId, body);
+      });
+    } else {
+      console.log('No device tokens found for this realtor.');
+    }
+  }
 }
+
     
-}
+
