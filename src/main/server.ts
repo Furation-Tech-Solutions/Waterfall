d09addca832cd632e0524cdfa181d035e9a4d0cc
MalikFuration19 <139318742+MalikFuration19@users.noreply.github.com>
2023-12-30 16:26:52 +0530

    Feature/notification on cron (#309)
    * feat: added push notification to expiredjob
    
    * fix: changes in push notification on cron
    
    * fix: updated push notification on expired job

--- src/main/server.ts
+++ src/main/server.ts
@@ -5,6 +5,7 @@ import ApiError from "@presentation/error-handling/api-error";
 import * as Message from "@presentation/error-handling/message-error";
 import { sequelize } from "@main/sequelizeClient";
 import { deleteStatusWithCron } from "@presentation/middlewares/cron/deleteUserCron";
+import { CronJob, notificationWithCron } from "@presentation/middlewares/cron/pushNotificationCron";
 
 const app = setupApp();
 
@@ -25,6 +26,9 @@ try {
   });
 
   deleteStatusWithCron();
+  const cronClass=new CronJob()
+  cronClass.expiredJobNotification()
+  // notificationWithCron()
 
 } catch (error) {
   console.log("error is this-", error, "error");
